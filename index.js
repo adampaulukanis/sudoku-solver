71147d56b0331397c25d85ad928cb37983ad1a54
Adam Paulukanis <adam.paulukanis@gmail.com>
2021-09-03 21:31:50 +0200

    Collect the empty positions
    saveEmptyPositions() should find all 0's in the board and save the
    coordinates to an array.

--- index.js
+++ index.js
@@ -7,3 +7,19 @@ module.exports.parseBoard = function (board) {
     });
   });
 };
+
+module.exports.saveEmptyPositions = function (board) {
+  var emptyPositions = [];
+
+  // Check every square for a zero
+  for (var y = 0; y < board.length; y++) {
+    for (var x = 0; x < board[y].length; x++) {
+      // If found 0, save the position
+      if (board[y][x] === 0) {
+        emptyPositions.push([y, x]);
+      }
+    }
+  }
+
+  return emptyPositions;
+};
